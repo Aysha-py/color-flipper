6a47cb56ebf6248258c249c0b662eb3fe5892bd0
Aysha Muhammed <m.aishat9890@gmail.com>
2021-11-23 08:11:22 -0800

    add

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ const button = document.getElementById("btn");
 button.addEventListener("click",function(){
     
     const randomNumber = getRandomNumber();
-    console.log(randomNumber);
     document.body.style.backgroundColor = colors[randomNumber];
 })
 
 function getRandomNumber(){
     return Math.floor(Math.random()*colors.length);
+}
+
+function colorsButton(){
+
+
+    const randomNumber = getRandomNumber();
+
+    document.body.style.backgroundColor = colors[randomNumber];
+}
+function plainButton(){
+    document.body.style.backgroundColor = "white"
 }
